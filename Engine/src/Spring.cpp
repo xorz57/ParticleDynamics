3c535cca520946b61b8198b0fb6f8cc29cb79f23
George Fotopoulos <xorz57@gmail.com>
2023-12-18 12:09:52 +0200

    Update

--- Engine/src/Spring.cpp
+++ Engine/src/Spring.cpp
@@ -2,9 +2,8 @@
 
 using namespace Engine;
 
-Spring::Spring(Particle &particle1, Particle &particle2, float restLength, float springConstant, float dampingConstant)
-    : mParticle1(particle1),
-      mParticle2(particle2),
-      mRestLength(restLength),
-      mSpringConstant(springConstant),
-      mDampingConstant(dampingConstant) {}
+Spring::Spring(Particle &particle1, Particle &particle2, float restLength, float springConstant, float dampingConstant) : mParticle1(particle1),
+                                                                                                                          mParticle2(particle2),
+                                                                                                                          mRestLength(restLength),
+                                                                                                                          mSpringConstant(springConstant),
+                                                                                                                          mDampingConstant(dampingConstant) {}

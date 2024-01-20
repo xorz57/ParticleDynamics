342d3947cefa2455b1566b7aa1af6101254451c5
George Fotopoulos <xorz57@gmail.com>
2024-01-20 20:18:49 +0200

    Refactor DoubleSpringPendulum

--- src/DoubleSpringPendulum.cpp
+++ src/DoubleSpringPendulum.cpp
@@ -3,14 +3,14 @@
 #include <glm/gtc/constants.hpp>
 
 DoubleSpringPendulum::DoubleSpringPendulum(const glm::vec2 &pivot, float length, float particleMass, float particleRadius) : SoftBody() {
-    particles.emplace_back(pivot, particleMass, particleRadius, true);
-
     constexpr float angle1 = glm::pi<float>() / 3.0f;
-    const glm::vec2 position1 = pivot + length * glm::vec2(glm::cos(angle1), glm::sin(angle1));
-    particles.emplace_back(position1, particleMass, particleRadius, false);
-
     constexpr float angle2 = glm::pi<float>() / 6.0f;
+
+    const glm::vec2 position1 = pivot + length * glm::vec2(glm::cos(angle1), glm::sin(angle1));
     const glm::vec2 position2 = position1 + length * glm::vec2(glm::cos(angle2), glm::sin(angle2));
+
+    particles.emplace_back(pivot, particleMass, particleRadius, true);
+    particles.emplace_back(position1, particleMass, particleRadius, false);
     particles.emplace_back(position2, particleMass, particleRadius, false);
 
     const auto distance = [](const Particle &particle1, const Particle &particle2) {

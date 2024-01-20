92a776213d94982d10d1e5dfe239aa2c2cf80674
George Fotopoulos <xorz57@gmail.com>
2024-01-20 20:05:05 +0200

    Inline expand CreateApplication()

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,19 +4,19 @@
 #include <cstdlib>
 #include <memory>
 
-std::unique_ptr<Application> CreateApplication() {
+int main() {
+    Logging::Init();
+
     ApplicationSettings applicationSettings;
     applicationSettings.width = 600U;
     applicationSettings.height = 600U;
-    applicationSettings.title = "Sandbox";
+    applicationSettings.title = "ParticlePhysics";
     applicationSettings.fixedDeltaTime = 1.0f / 64.0f;
     applicationSettings.timeScale = 4.0f;
-    return std::make_unique<Application>(applicationSettings);
-}
 
-int main() {
-    Logging::Init();
-    std::unique_ptr<Application> application = CreateApplication();
+    std::unique_ptr<Application> application = std::make_unique<Application>(applicationSettings);
+
     application->Run();
+
     return EXIT_SUCCESS;
 }

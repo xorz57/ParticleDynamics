18c8f4596c28ef35083e6d3c81ef148168ef0f25
George Fotopoulos <xorz57@gmail.com>
2024-01-20 19:58:54 +0200

    Move CreateApplication()

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,19 @@
 #include "Application.hpp"
 #include "Logging.hpp"
+#include "Sandbox.hpp"
 
 #include <cstdlib>
 #include <memory>
 
-std::unique_ptr<Application> CreateApplication();
+std::unique_ptr<Application> CreateApplication() {
+    ApplicationSettings applicationSettings;
+    applicationSettings.width = 600U;
+    applicationSettings.height = 600U;
+    applicationSettings.title = "Sandbox";
+    applicationSettings.fixedDeltaTime = 1.0f / 64.0f;
+    applicationSettings.timeScale = 4.0f;
+    return std::make_unique<Sandbox>(applicationSettings);
+}
 
 int main() {
     Logging::Init();
--- src/Sandbox.cpp
+++ src/Sandbox.cpp
@@ -4,13 +4,3 @@
 Sandbox::Sandbox(const ApplicationSettings &applicationSettings) : Application(applicationSettings) {
     PushLayer(std::make_unique<SandboxLayer>("SandboxLayer"));
 }
-
-std::unique_ptr<Application> CreateApplication() {
-    ApplicationSettings applicationSettings;
-    applicationSettings.width = 600U;
-    applicationSettings.height = 600U;
-    applicationSettings.title = "Sandbox";
-    applicationSettings.fixedDeltaTime = 1.0f / 64.0f;
-    applicationSettings.timeScale = 4.0f;
-    return std::make_unique<Sandbox>(applicationSettings);
-}

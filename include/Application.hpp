7a2c69e378b71f661040737bee19e25161bfce82
George Fotopoulos <xorz57@gmail.com>
2024-01-25 01:30:08 +0200

    Refactor Application

--- include/Application.hpp
+++ include/Application.hpp
@@ -3,8 +3,6 @@
 #include "ApplicationSettings.hpp"
 #include "SoftBody.hpp"
 
-#include <memory>
-
 class Application {
 public:
     explicit Application(ApplicationSettings applicationSettings);
@@ -13,5 +11,5 @@ public:
 private:
     ApplicationSettings mApplicationSettings;
     glm::vec2 mGravitationalAcceleration{0.0f, 10.0f};
-    std::vector<std::unique_ptr<SoftBody>> mSoftBodies;
+    std::vector<SoftBody> mSoftBodies;
 };
--- src/Application.cpp
+++ src/Application.cpp
@@ -22,11 +22,11 @@
 #include <implot.h>
 
 Application::Application(ApplicationSettings applicationSettings) : mApplicationSettings(std::move(applicationSettings)) {
-    mSoftBodies.emplace_back(std::make_unique<DoubleSpringPendulum>(glm::vec2(200.0f, 0.0f), 50.0f, 2.0f, 4.0f));
-    mSoftBodies.emplace_back(std::make_unique<SoftBody1>(glm::vec2(100.0f, 100.0f), 50.0f, 2.0f, 4.0f));
-    mSoftBodies.emplace_back(std::make_unique<SoftBody2>(glm::vec2(300.0f, 200.0f), 50.0f, 2.0f, 4.0f));
-    mSoftBodies.emplace_back(std::make_unique<SoftBody3>(glm::vec2(500.0f, 300.0f), 50.0f, 2.0f, 4.0f, 8));
-    mSoftBodies.emplace_back(std::make_unique<SpringPendulum>(glm::vec2(400.0f, 0.0f), 100.0f, 2.0f, 4.0f));
+    mSoftBodies.emplace_back(DoubleSpringPendulum(glm::vec2(200.0f, 0.0f), 50.0f, 2.0f, 4.0f));
+    mSoftBodies.emplace_back(SoftBody1(glm::vec2(100.0f, 100.0f), 50.0f, 2.0f, 4.0f));
+    mSoftBodies.emplace_back(SoftBody2(glm::vec2(300.0f, 200.0f), 50.0f, 2.0f, 4.0f));
+    mSoftBodies.emplace_back(SoftBody3(glm::vec2(500.0f, 300.0f), 50.0f, 2.0f, 4.0f, 8));
+    mSoftBodies.emplace_back(SpringPendulum(glm::vec2(400.0f, 0.0f), 100.0f, 2.0f, 4.0f));
 }
 
 void Application::Run() {
@@ -71,8 +71,8 @@ void Application::Run() {
         accumulator += timeScale * deltaTime;
         while (accumulator > fixedDeltaTime) {
             float dt = fixedDeltaTime.asSeconds();
-            for (const std::unique_ptr<SoftBody> &softBody: mSoftBodies) {
-                for (Spring &spring: softBody->springs) {
+            for (SoftBody &softBody: mSoftBodies) {
+                for (Spring &spring: softBody.springs) {
                     const glm::vec2 dPosition = spring.mParticle2.position - spring.mParticle1.position;
                     const glm::vec2 dVelocity = spring.mParticle2.velocity - spring.mParticle1.velocity;
                     if (glm::length(dPosition) != 0) {
@@ -81,7 +81,7 @@ void Application::Run() {
                         spring.mParticle2.force -= force;
                     }
                 }
-                for (Particle &particle: softBody->particles) {
+                for (Particle &particle: softBody.particles) {
                     const glm::vec2 gravitationalForce = particle.mass * mGravitationalAcceleration;
                     particle.force += gravitationalForce;
                     if (!particle.pinned) {
@@ -100,8 +100,8 @@ void Application::Run() {
 
         window.clear(sf::Color(25, 25, 25));
 
-        for (const std::unique_ptr<SoftBody> &softBody: mSoftBodies) {
-            for (const Spring &spring: softBody->springs) {
+        for (const SoftBody &softBody: mSoftBodies) {
+            for (const Spring &spring: softBody.springs) {
                 sf::VertexArray line(sf::Lines, 2);
                 line[0].position.x = spring.mParticle1.position.x;
                 line[0].position.y = spring.mParticle1.position.y;
@@ -111,7 +111,7 @@ void Application::Run() {
                 line[1].color = sf::Color(206, 208, 206);
                 window.draw(line);
             }
-            for (const Particle &particle: softBody->particles) {
+            for (const Particle &particle: softBody.particles) {
                 sf::CircleShape shape;
                 shape.setRadius(particle.radius);
                 shape.setOrigin(particle.radius, particle.radius);

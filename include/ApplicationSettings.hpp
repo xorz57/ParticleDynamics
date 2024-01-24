146c3806148096bdce2a8f7ab11caffe58b99e9f
George Fotopoulos <xorz57@gmail.com>
2024-01-25 01:52:22 +0200

    Minor refactoring

--- include/ApplicationSettings.hpp
+++ include/ApplicationSettings.hpp
@@ -3,9 +3,9 @@
 #include <string>
 
 struct ApplicationSettings {
-    unsigned int width = 0U;
-    unsigned int height = 0U;
+    unsigned int width{0U};
+    unsigned int height{0U};
     std::string title;
-    float fixedDeltaTime = 1.0f / 64.0f;
-    float timeScale = 1.0f;
+    float fixedDeltaTime{1.0f / 64.0f};
+    float timeScale{1.0f};
 };

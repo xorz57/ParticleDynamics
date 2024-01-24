535e72c0dc65709db601367e8e2584618606ce48
George Fotopoulos <xorz57@gmail.com>
2024-01-25 01:03:24 +0200

    Remove unused include directive

--- include/SoftBody3.hpp
+++ include/SoftBody3.hpp
@@ -2,8 +2,6 @@
 
 #include "SoftBody.hpp"
 
-#include <cstdint>
-
 struct SoftBody3 : SoftBody {
     SoftBody3(const glm::vec2 &center, float radius, float particleMass, float particleRadius, std::size_t numParticles);
 };
